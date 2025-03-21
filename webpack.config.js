7f7fd435830668b33b29e5954feb8337348c7d5a
MarianoMigueles <164581983+MarianoMigueles@users.noreply.github.com>
2025-03-21 16:38:28 -0300

    FIX(webpack.config): bad write route.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './code/JS/script.js',
+  entry: './code/js/script.js',
   output: {
     filename: 'scripts.min.js',
     path: path.resolve(__dirname, 'dist'),
